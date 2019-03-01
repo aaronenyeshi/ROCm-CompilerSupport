01fb24848c7d275161a023a230662f131b2a4024
Aaron Enye Shi <enye.shi@gmail.com>
2019-03-01 18:57:52 +0000

    More working parsing for HCC binary!

--- lib/comgr/test/metadata_yaml_test.c
+++ lib/comgr/test/metadata_yaml_test.c
@@ -74,22 +74,40 @@ typedef struct Kernel_Attr {
   char *runtime_handle;
 } Kernel_Attr;
 
-typedef struct Kernel {
+typedef struct Kernel_Metadata {
   char *name;
   char *symbol_name;
   char *language;
   int language_version_major;
   int language_version_minor;
+  int num_attrs;
+  int num_args;
+  int num_code_props;
   Kernel_Attr *attrs;
   Kernel_Arg *args;
   Kernel_Code_Prop *code_props;
-} Kernel;
+} Kernel_Metadata;
 
 typedef struct Program_Metadata {
   int version_major;
   int version_minor;
-  Kernel *kernels;
+  int num_kernels;
+  Kernel_Metadata *kernels;
 } Program_Metadata;
+
+char* create_string_from_string_node(amd_comgr_metadata_node_t printNode) {
+    amd_comgr_status_t hcc_stat;
+    size_t printSize;
+    char *printString;
+    hcc_stat = amd_comgr_get_metadata_string(printNode, &printSize, NULL);
+    checkError(hcc_stat, "amd_comgr_get_metadata_string");
+    printf("printSize: %lu\n", printSize);
+    printString = (char*)malloc(printSize);
+    hcc_stat = amd_comgr_get_metadata_string(printNode, &printSize, printString);
+    checkError(hcc_stat, "amd_comgr_get_metadata_string");
+    printf("printString: %s\n", printString);
+    return printString;
+}
 
 int main(int argc, char *argv[]) {
   char *arg = NULL;
@@ -199,21 +217,14 @@ int main(int argc, char *argv[]) {
     }
     hcc_stat = amd_comgr_index_list_metadata(metaLookupHcc, 0, &printNode);
     checkError(hcc_stat, "amd_comgr_index_list_metadata");
-    hcc_stat = amd_comgr_get_metadata_string(printNode, &sizeHcc, NULL);
-    checkError(hcc_stat, "amd_comgr_get_metadata_string");
-    printString = (char*)malloc(sizeHcc);
-    hcc_stat = amd_comgr_get_metadata_string(printNode, &sizeHcc, printString);
-    checkError(hcc_stat, "amd_comgr_get_metadata_string");
-    program.version_major=atoi(printString);
+    printString = create_string_from_string_node(printNode);
+    if (printString)
+      program.version_major=atoi(printString);
     free(printString);
 
     hcc_stat = amd_comgr_index_list_metadata(metaLookupHcc, 1, &printNode);
     checkError(hcc_stat, "amd_comgr_index_list_metadata");
-    hcc_stat = amd_comgr_get_metadata_string(printNode, &sizeHcc, NULL);
-    checkError(hcc_stat, "amd_comgr_get_metadata_string");
-    printString = (char*)malloc(sizeHcc);
-    hcc_stat = amd_comgr_get_metadata_string(printNode, &sizeHcc, printString);
-    checkError(hcc_stat, "amd_comgr_get_metadata_string");
+    printString = create_string_from_string_node(printNode);
     program.version_minor=atoi(printString);
     free(printString);
 
@@ -228,13 +239,64 @@ int main(int argc, char *argv[]) {
 
     hcc_stat = amd_comgr_get_metadata_list_size(metaLookupHcc, &sizeHcc);
     checkError(status, "amd_comgr_get_metadata_list_size");
-    program.kernels = (Kernel*) malloc(sizeHcc * sizeof(Kernel));
-    for (int i = 0; i < sizeHcc; i++) {
+    program.kernels = (Kernel_Metadata*) malloc(sizeHcc * sizeof(Kernel_Metadata));
+    program.num_kernels = sizeHcc;
+    printf("num_kernels: %lu\n", sizeHcc);
+    for (int i = 0; i < program.num_kernels; i++) {
+      Kernel_Metadata *kern_md = &program.kernels[i];
+      kern_md->num_attrs=0;kern_md->num_args=0;kern_md->num_code_props=0;
+
       amd_comgr_metadata_node_t kernelMap;
       hcc_stat = amd_comgr_index_list_metadata(metaLookupHcc, i, &kernelMap);
       checkError(hcc_stat, "amd_comgr_index_list_metadata");
 
-      
+      hcc_stat = amd_comgr_metadata_lookup(kernelMap, "Name", &printNode);
+      checkError(hcc_stat, "amd_comgr_metadata_lookup");
+      kern_md->name = create_string_from_string_node(printNode);
+
+      hcc_stat = amd_comgr_metadata_lookup(kernelMap, "SymbolName", &printNode);
+      checkError(hcc_stat, "amd_comgr_metadata_lookup");
+      kern_md->symbol_name = create_string_from_string_node(printNode);
+
+      amd_comgr_metadata_node_t kernAttrMap;
+      hcc_stat = amd_comgr_metadata_lookup(kernelMap, "Attrs", &kernAttrMap);
+      if (hcc_stat == AMD_COMGR_STATUS_SUCCESS ) {
+        kern_md->attrs = (Kernel_Attr*) malloc(sizeof(Kernel_Attr));
+        kern_md->num_attrs = 1;
+        for (int k_at = 0; k_at < kern_md->num_attrs; k_at++) {
+          Kernel_Attr *kern_at = &kern_md->attrs[k_at];
+        }
+      }
+      printf("  num_attrs: %d\n", kern_md->num_attrs);
+
+      amd_comgr_metadata_node_t kernArgMap;
+      hcc_stat = amd_comgr_metadata_lookup(kernelMap, "Args", &kernArgMap);
+      if (hcc_stat == AMD_COMGR_STATUS_SUCCESS ) {
+        hcc_stat = amd_comgr_get_metadata_list_size(kernArgMap, &sizeHcc);
+        checkError(status, "amd_comgr_get_metadata_list_size");
+        kern_md->args = (Kernel_Arg*) malloc(sizeHcc * sizeof(Kernel_Arg));
+        kern_md->num_args = sizeHcc;
+        for (int k_ar = 0; k_ar < kern_md->num_args; k_ar++) {
+          Kernel_Arg *kern_ar = &kern_md->args[k_ar];
+        }
+      }
+      printf("  num_args: %d\n", kern_md->num_args);
+
+      amd_comgr_metadata_node_t kernCodePropMap;
+      hcc_stat = amd_comgr_metadata_lookup(kernelMap, "CodeProps", &kernCodePropMap);
+      if (hcc_stat == AMD_COMGR_STATUS_SUCCESS ) {
+        kern_md->num_code_props = 1;
+        kern_md->code_props = (Kernel_Code_Prop*) malloc(sizeof(Kernel_Code_Prop));
+        for (int k_cp = 0; k_cp < kern_md->num_code_props; k_cp++) {
+          Kernel_Code_Prop *kern_cp = &kern_md->code_props[k_cp];
+          hcc_stat = amd_comgr_metadata_lookup(kernCodePropMap, "KernargSegmentSize", &printNode);
+          checkError(hcc_stat, "amd_comgr_metadata_lookup");
+          printString = create_string_from_string_node(printNode);
+          kern_cp->kernarg_segment_size = atoi(printString);
+          free(printString);
+        }
+      }
+      printf("  num_code_props: %d\n", kern_md->num_code_props);
     }
 
     printf("  Clean up Aaron HCC ...\n\n");
@@ -243,9 +305,48 @@ int main(int argc, char *argv[]) {
     hcc_stat = amd_comgr_destroy_metadata(printNode);
     checkError(hcc_stat, "amd_comgr_destroy_metadata");
 
+    char indent[50];
+    strcpy(indent, "  "); indent[2]='\0';
     printf("Printing all the Metadata Structure\n");
     printf("Program Metadata\n");
-    printf("  Version: %d , %d\n", program.version_major, program.version_minor);
+    printf("%sVersion: %d , %d\n", indent, program.version_major, program.version_minor);
+    for (int k = 0; k < program.num_kernels; k++) {
+      strcat(indent, "  ");
+      Kernel_Metadata *kern_md = &program.kernels[k];
+      printf("%sName: %s\n", indent, kern_md->name);
+      printf("%sSymbolName: %s\n", indent, kern_md->symbol_name);
+      for (int k_at = 0; k_at < kern_md->num_attrs; k_at++) {
+        Kernel_Attr *kern_at = &kern_md->attrs[k_at];
+      }
+      for (int k_ar = 0; k_ar < kern_md->num_args; k_ar++) {
+        Kernel_Arg *kern_ar = &kern_md->args[k_ar];
+      }
+      for (int k_cp = 0; k_cp < kern_md->num_code_props; k_cp++) {
+        Kernel_Code_Prop *kern_cp = &kern_md->code_props[k_cp];
+        printf("%sKernargSegmentSize: %d\n", indent, kern_cp->kernarg_segment_size);
+      }
+    }
+
+    // Clean up everything I've malloc
+    for (int k = 0; k < program.num_kernels; k++) {
+      Kernel_Metadata *kern_md = &program.kernels[k];
+      for (int k_at = 0; k_at < kern_md->num_attrs; k_at++) {
+        Kernel_Attr *kern_at = &kern_md->attrs[k_at];
+      }
+      if(kern_md->attrs && kern_md->num_attrs)
+        free(kern_md->attrs);
+      for (int k_ar = 0; k_ar < kern_md->num_args; k_ar++) {
+        Kernel_Arg *kern_ar = &kern_md->args[k_ar];
+      }
+      if(kern_md->args && kern_md->num_args)
+        free(kern_md->args);
+      for (int k_cp = 0; k_cp < kern_md->num_code_props; k_cp++) {
+        Kernel_Code_Prop *kern_cp = &kern_md->code_props[k_cp];
+      }
+      if(kern_md->code_props && kern_md->num_code_props)
+        free(kern_md->code_props);
+    }
+    free(program.kernels);
   }
 
   {
